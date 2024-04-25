93e90f5524fc7ba4e081aef54b5038d1c1554506
Lena Merkli <lena.merkli@protonmail.com>
2024-04-25 18:23:53 +0200

    Various improvements to the video to frames converter.

--- data/video_to_frames.py
+++ data/video_to_frames.py
@@ -17,11 +17,11 @@ def relative_path(string: str) -> str:
 
 def main():
     for file in tqdm(listdir(relative_path('videos'))):
-        if file.rsplit('.', 1)[1].strip().lower() in VIDEO_EXTENSIONS:
+        if file.split('.')[-1].strip().lower() in VIDEO_EXTENSIONS:
             (
                 ffmpeg
                 .input(relative_path(f"videos/{file}"))
-                .output(relative_path('frames/frame%05d.png'))
+                .output(relative_path(f"frames/{file.rsplit('.', 1)[0]}_%04d.png"), loglevel='quiet')
                 .run()
             )
 
