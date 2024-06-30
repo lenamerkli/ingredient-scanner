a5bc7b501ff964ab6a24924cdf3b00b39869e827
Lena Merkli <lena.merkli@protonmail.com>
2024-06-30 10:21:50 +0200

    Add secret research vision model

--- ocr.py
+++ ocr.py
@@ -1,53 +1,41 @@
-import easyocr
 import requests
 import base64
-import torch
 from dotenv import load_dotenv
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
-from general import relative_path, tqdm, os, json, Image
+from general import relative_path, os, json, Image
 
 MAX_SIZE = 5_000_000
-# ENGINE = ['easyocr']
-ENGINE = ['anthropic', 'claude-3-5-sonnet-20240620', 'claude_3_5_sonnet']
-# ENGINE = ['Qwen-VL', 'Qwen-VL-Chat']
-# ENGINE = ['MiniCPM-V-2']
-SEED = 2024 - 7 - 28
+# ENGINE = ['anthropic', 'claude-3-5-sonnet-20240620', 'claude_3_5_sonnet']
+ENGINE = ['local', 'llama_cpp/v2/vision', 'qwen-vl-next_b2583']
 with open(relative_path('data2/prompt.md'), 'r', encoding='utf-8') as _f:
     PROMPT = _f.read()
 
 load_dotenv()
 
 
+def decrease_size(input_path, output_path):
+    with Image.open(input_path) as img:
+        original_size = os.path.getsize(input_path)
+        if original_size <= MAX_SIZE:
+            print("Image is already below the maximum size.")
+            return True
+        width, height = img.size
+        while width > 0 and height > 0:
+            img_resized = img.resize((width, height), Image.Resampling.LANCZOS)
+            img_resized.save(output_path, format=output_path.split('.')[-1].upper())
+            if os.path.getsize(output_path) <= MAX_SIZE:
+                print(f"Reduced image size to {os.path.getsize(output_path)} bytes.")
+                break
+            width, height = int(width * 0.9), int(height * 0.9)
+        if os.path.getsize(output_path) > MAX_SIZE:
+            raise ValueError("Could not reduce PNG size below max_size by reducing resolution.")
+
+
 def main():
-    torch.manual_seed(SEED)
-    if ENGINE[0] == 'easyocr':
-        reader = easyocr.Reader(['de', 'fr', 'en'], gpu=True)
-        for file in tqdm(os.listdir(relative_path('data2/frames'))):
-            if file.endswith('.png'):
-                result = reader.readtext(relative_path(f'data2/frames/{file}'))
-                with open(relative_path(f"data2/frames_ocr/{file.rsplit('.', 1)[0]}.txt"), 'w', encoding='utf-8') as f:
-                    f.write('\n'.join([i[1] for i in result]))
-    elif ENGINE[0] == 'anthropic':
+    if ENGINE[0] == 'anthropic':
         file = input('Enter file name without extension: ')
         input_path = relative_path(f'data2/frames/{file}.png')
         output_path = relative_path(f'tmp/frames_claude/{file}.webp')
-        with Image.open(input_path) as img:
-            original_size = os.path.getsize(input_path)
-            if original_size <= MAX_SIZE:
-                print("Image is already below the maximum size.")
-                return True
-
-            # Start reducing the size by halving the resolution until it's below max_size
-            width, height = img.size
-            while width > 0 and height > 0:
-                width, height = int(width * 0.9), int(height * 0.9)
-                img_resized = img.resize((width, height), Image.Resampling.LANCZOS)
-                img_resized.save(output_path, format='WEBP')
-                if os.path.getsize(output_path) <= MAX_SIZE:
-                    print(f"Reduced image size to {os.path.getsize(output_path)} bytes.")
-                    break
-            if os.path.getsize(output_path) > MAX_SIZE:
-                raise ValueError("Could not reduce PNG size below max_size by reducing resolution.")
+        decrease_size(input_path, output_path)
         with open(output_path, 'rb') as f:
             image = base64.b64encode(f.read()).decode('utf-8')
         response = requests.post(
@@ -63,19 +51,19 @@ def main():
                 'messages': [
                     {
                         'role': 'user', 'content': [
-                        {
-                            'type': 'image',
-                            'source': {
-                                'type': 'base64',
-                                'media_type': 'image/webp',
-                                'data': image,
+                            {
+                                'type': 'image',
+                                'source': {
+                                    'type': 'base64',
+                                    'media_type': 'image/webp',
+                                    'data': image,
+                                },
                             },
-                        },
-                        {
-                            'type': 'text',
-                            'text': PROMPT,
-                        },
-                    ],
+                            {
+                                'type': 'text',
+                                'text': PROMPT,
+                            },
+                        ],
                     },
                 ],
             }),
@@ -84,49 +72,34 @@ def main():
         with open(relative_path(f"data2/frames_claude/{file}.json"), 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=4)
         print(data)
-    elif ENGINE[0] == 'Qwen-VL':
-        tokenizer = AutoTokenizer.from_pretrained(f"Qwen/{ENGINE[1]}", trust_remote_code=True)
-        model = AutoModelForCausalLM.from_pretrained(f"Qwen/{ENGINE[1]}", device_map='cuda', trust_remote_code=True,
-                                                     bf16=True).eval()
-        for file in tqdm(os.listdir(relative_path('data2/frames'))):
-            if file.endswith('.png') and not os.path.exists(
-                    relative_path(f"data2/frames_qwen_vl/{file.rsplit('.', 1)[0]}.json")):
-                query = tokenizer.from_list_format([
-                    {'image': relative_path(f'data2/frames/{file}')},
-                    {'text': PROMPT},
-                ])
-                response, history = model.chat(tokenizer, query=query, history=None)
-                with open(relative_path(f"data2/frames_qwen_vl/{file.rsplit('.', 1)[0]}.txt"), 'w',
-                          encoding='utf-8') as f:
-                    f.write(str(response) + '\n')
-                print(response)
-    elif ENGINE[0] == 'MiniCPM-V-2':
-        model = AutoModel.from_pretrained('openbmb/MiniCPM-V-2', trust_remote_code=True, torch_dtype=torch.bfloat16)
-        model = model.to(device='cuda', dtype=torch.bfloat16)
-        tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-V-2', trust_remote_code=True)
-        model.eval()
-        for file in tqdm(os.listdir(relative_path('data2/frames'))):
-            if file.endswith('.png') and not os.path.exists(
-                    relative_path(f"data2/frames_minicpm_v2/{file.rsplit('.', 1)[0]}.json")):
-                image = Image.open(relative_path(f'data2/frames/{file}'))
-                msgs = [
-                    {
-                        'role': 'user',
-                        'content': PROMPT,
-                    }
-                ]
-                response, context, _ = model.chat(
-                    image=image,
-                    msgs=msgs,
-                    context=None,
-                    tokenizer=tokenizer,
-                    sampling=True,
-                    temperature=0.7
-                )
-                with open(relative_path(f"data2/frames_minicpm_v2/{file.rsplit('.', 1)[0]}.txt"), 'w',
-                          encoding='utf-8') as f:
-                    f.write(str(response) + '\n')
-                print(response)
+    elif ENGINE[0] == 'local':
+        file = input('Enter file name without extension: ')
+        input_path = relative_path(f'data2/frames/{file}.png')
+        output_path = relative_path(f'tmp/frames_local/{file}.webp')
+        decrease_size(input_path, output_path)
+        response = requests.post(
+            url=f"http://127.0.0.1:11434/{ENGINE[1]}",
+            headers={
+                'x-version': '2024-05-21',
+                'content-type': 'application/json',
+            },
+            data=json.dumps({
+                'task': PROMPT,
+                'model': ENGINE[2],
+                'image_path': output_path,
+            }),
+        )
+        data: str = response.json()['text']
+        with open(relative_path(f"data2/frames_local/{file}.txt"), 'w', encoding='utf-8') as f:
+            f.write(data)
+        print(data + '\n\n')
+        data = '{' + data.split('{', 1)[-1]
+        data = data.rsplit('}', 1)[0] + '}'
+        data = data.replace('\\n', '\n')
+        data = json.loads(data)
+        with open(relative_path(f"data2/frames_local/{file}.json"), 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4)
+        print(data)
 
 
 if __name__ == '__main__':
